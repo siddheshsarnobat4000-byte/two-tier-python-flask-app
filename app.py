d26b446a218c2d6ee9436f06926fe12835684302
siddheshsarnobat4000-byte <siddheshsarnobat4000@gmail.com>
2026-03-28 11:34:52 +0000

    removed mysql data folder

--- app.py
+++ app.py
@@ -8,22 +8,33 @@ app = Flask(__name__)
 app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST', 'localhost')
 app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', 'default_user')
 app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', 'default_password')
-app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', 'default_db')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DATABASE', 'sid-db')
 
 # Initialize MySQL
 mysql = MySQL(app)
 
+import time
+
 def init_db():
-    with app.app_context():
-        cur = mysql.connection.cursor()
-        cur.execute('''
-        CREATE TABLE IF NOT EXISTS messages (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            message TEXT
-        );
-        ''')
-        mysql.connection.commit()  
-        cur.close()
+    for i in range(10):
+        try:
+            with app.app_context():
+                cur = mysql.connection.cursor()
+                cur.execute('''
+                CREATE TABLE IF NOT EXISTS messages (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    message TEXT
+                );
+                ''')
+                mysql.connection.commit()
+                cur.close()
+            print("Database initialized")
+            return
+        except Exception as e:
+            print("MySQL not ready, retrying...")
+            time.sleep(3)
+
+    raise Exception("Could not connect to MySQL")
 
 @app.route('/')
 def hello():
